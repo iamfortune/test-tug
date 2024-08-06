415b26edc60b807a023cfe52e5a60e349175112a
Fortune Ikechi <ikechifortune@gmail.com>
2024-08-06 12:52:02 +0100

    test
    Signed-off-by: Fortune Ikechi <ikechifortune@gmail.com>

--- services/wealthy-animal/routes/root.js
+++ services/wealthy-animal/routes/root.js
@@ -2,7 +2,10 @@
 'use strict'
 /** @param {import('fastify').FastifyInstance} fastify */
 module.exports = async function (fastify, opts) {
-  fastify.get('/example', async (request, reply) => {
-    return { hello: fastify.example }
-  })
+  fastify.get("/example", async (request, reply) => {
+    return { hello: fastify.example };
+  });
+    fastify.post("/great-ne", async (request, reply) => {
+      return { bye: fastify.example };
+    });
 }
